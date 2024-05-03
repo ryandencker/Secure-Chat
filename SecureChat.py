298758f0e4b2c74a705400ce5a469f59ea4c76ec
Ryan Dencker <ryanddencker@gmail.com>
2024-05-03 00:38:42 -0700

    added user ids

--- SecureChat.py
+++ SecureChat.py
@@ -1,15 +1,29 @@
 import bcrypt
 import pwinput
+import random
 
+
+def place_online(username):
+    f = open("online.txt", "a")
+    f.write(f"{username} ")
 
 def create():
+    #get username and password
     username = input("Please enter your username: ")
     password = pwinput.pwinput(prompt="Please enter your password: ")
+
+    #hash password
     hashedPassword = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+    #generate random 5 digit user ID
+    user_id = random.randint(10000, 99999)
+    
+
     f = open("db.txt", "a")
-    f.write(f"{username} {hashedPassword.decode('utf-8')}\n")
+    f.write(f"{username} {hashedPassword.decode('utf-8')} {user_id}\n")
     f.close
-    print("Account has been created!")
+    print("\nAccount has been created!")
+    print("Your User ID is: ", user_id)
 
 
 
@@ -19,11 +33,12 @@ def login():
     hashedPassword = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     f = open("db.txt", "r")
     for line in f:
-        stored_username, stored_password = line.strip().split(" ")
+        stored_username, stored_password, user_id = line.strip().split(" ")
         if username == stored_username:
             if bcrypt.checkpw(password.encode('utf-8'), stored_password.encode('utf-8')):
                 f.close()
                 print("Welcome ", username)
+                place_online(username)
                 exit()
     f.close()
     print("Your username or password is wrong. Try again")
@@ -31,14 +46,18 @@ def login():
 
 def main():
     print("\n-------WELCOME TO SECURE CHAT-------\n\n")
-    user_input = input("Press 1 to create an account or press 2 to log into an existing account: ")
 
-    if user_input == '1':
-        create()
-    elif user_input == '2':
-        login()
-    else:
-        print("Invalid choice. Please try again")
+    while(True):
+        user_input = input("Press 1 to create an account, 2 to log into an existing account, or 3 to quit: ")
+
+        if user_input == '1':
+            create()
+        elif user_input == '2':
+            login()
+        elif user_input == '3':
+            break
+        else:
+            print("Invalid choice. Please try again")
 
 if __name__ == "__main__":
     main()
