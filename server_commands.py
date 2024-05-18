4d22d1c46d7a2dadc19fad3f0fd79b9f66230746
LovelySpartan <hart224444@gmail.com>
2024-05-17 17:04:06 -0700

    fixed remove and check existing user

--- server_commands.py
+++ server_commands.py
@@ -4,6 +4,20 @@ import pwinput
 import random
 import sys
 
+online_users = {}
+
+
+def handle_client(connection_socket, addr):
+    try:
+        accout_option(connection_socket)
+        command_handler(connection_socket)
+    except Exception as e:
+        print(f"An error occurred with {addr}: {e}")
+    finally:
+        remove_from_online(connection_socket)
+        connection_socket.close()
+
+
 def accout_option(client_command, connection_socket):
 
     while (True):
@@ -16,8 +30,9 @@ def accout_option(client_command, connection_socket):
             login(connection_socket)
             break
         elif client_command == b'3':
-            #this needs to be worked on
-            print("disconnect client")
+            connection_socket.send("Disconnecting...\n".encode())
+            connection_socket.close()
+            return
         else:
             invalid = "Invalid choice. Please try again\n"
             connection_socket.send(invalid.encode())
@@ -34,7 +49,8 @@ def command_handler(connection_socket):
     # Get the "arguments from the client and split them up"
 
     while (True):
-        connection_socket.send(("\nWelcome User!\n\nWhat would you like to do? \n\nPress 1 to see who is online\nPress 2 to connect to someone online\nPress 3 to disconnect").encode())
+        connection_socket.send(("\nWelcome User!\n\nWhat would you like to do? \n\nPress 1 to see who is 
+online\nPress 2 to connect to someone online\nPress 3 to disconnect").encode())
         client_command = connection_socket.recv(1024)
         print("here is what is being passes to command handler")
         print(client_command)
@@ -45,9 +61,11 @@ def command_handler(connection_socket):
         elif client_command == b'2':
             connect_client(connection_socket)
         elif client_command == b'3':
-            #this needs to be worked on
-            print("disconnect client")
-    
+            connection_socket.send("Disconnecting...\n".encode())
+            remove_from_online(connection_socket)
+            print(connection_socket)
+            connection_socket.close()
+            break    
 def create_account(connection_socket):
 
     #get username and password
@@ -55,6 +73,14 @@ def create_account(connection_socket):
     connection_socket.send(username_msg.encode())
     username = connection_socket.recv(1024).decode()
     print(username)
+    with open("db.txt", "r") as f:
+         for line in f:
+             stored_username, _, _ = line.strip().split(" ")
+             if username == stored_username:
+                 connection_socket.send("Username already exists. Please choose a different one.\n".encode())
+                 return
+
+
     password_msg = "Please enter your password: "
     connection_socket.send(password_msg.encode())
     password = connection_socket.recv(1024).decode()
@@ -87,6 +113,8 @@ def create_account(connection_socket):
     #send message to client
     created_msg = "Account has been created!\n"
     connection_socket.send(created_msg.encode())
+    place_online(username)
+    online_users[connection_socket] = username
 
 def login(connection_socket):
 
@@ -111,6 +139,7 @@ def login(connection_socket):
                     user_found = True
                     #connection_socket.send(("Welcome " + username + "!").encode())
                     place_online(username)
+                    online_users[connection_socket] = username
                     return
         f.close()
         if(not user_found):
@@ -144,3 +173,15 @@ def connect_client(connection_socket):
     user_id = connection_socket.recv(1024).decode()
     print("user would like to connect to ", user_id)
     connection_socket.send(("wow you are so awesome!!\nThis still needs to be worked on\n\n").encode())
+
+def remove_from_online(connection_socket):
+    username = online_users.pop(connection_socket, None)
+    if username:
+        with open("online.txt", "r") as f:
+            lines = f.readlines()
+        with open("online.txt", "w") as f:
+            for line in lines:
+                if not line.startswith(username):
+                    f.write(line)
+        print(f"{username} has been removed from online list.")
+
