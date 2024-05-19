d4f07f9d91fe8f772ad0fc7c0996217dc98959b9
Brody Whelan <bwhelan212@csu.fullerton.edu>
2024-05-19 02:28:20 -0700

    pub/priv keys for client sent to files, occassional ciphertext length error w/symm key

--- client.py
+++ client.py
@@ -1,75 +1,108 @@
 import socket
 import threading
-# from Cryptodome.Signature import DSS
-# from Cryptodome.PublicKey import DSA
-# from Cryptodome.Hash import SHA256
-# key = DSA.generate(2048)
-
-#     # get private key
-# f = open("dsa_private_key.pem", "wb")
-# f.write(key.export_key())
-# f.close()
-    
-#     # get public key
-# f = open("dsa_public_key.pem", "wb")
-# # Get the corresponding public key
-# pubKey =  key.publickey().export_key()
-# f.write(pubKey)
-# f.close()
-#generate rsa key for pub key crypto
+import random
 from Cryptodome.PublicKey import RSA
+from Cryptodome.Cipher import PKCS1_OAEP
+from Cryptodome.Util.Padding import unpad
 
+class Client:
+    def __init__(self):
+        #symm key is accessible to all functions in client class
+        self.dec_symm_key = None
+    def receive_messages(self,client_socket,priv_file_name):
+        # receive symmetric key
+        
+        while True:
+            try:
+                # receive encrypted symmetric key
+                # occasional ciphertext length error, not sure how to fix
+                message = client_socket.recv(8192)
+                # print(message, '\n')
+                if message:
+                    private_key = RSA.import_key(open(priv_file_name).read())
 
-private_key = None
-def receive_messages(client_socket):
-    while True:
-        try:
-            message = client_socket.recv(1024).decode()
-            if message:
-                print("\n" + message)  # Print the incoming message
-                print("Secure-Chat> ", end='', flush=True)  # Reprint the prompt
-            else:
-                break
-        except Exception as e:
-            print(f"An error occurred while receiving messages: {e}")
-            break
-
-def send_messages(client_socket):
-    # with open("dsa_public_key.pem", "rb") as f:
-    #     public_key = f.read()
-    #     client_socket.send(public_key)
-    while True:
-        try:
-            user_input = input("Secure-Chat> ")
-            if user_input:
-                client_socket.send(user_input.encode())
-                if user_input == "3":  # Disconnect command
+                    dec_cipher_rsa = PKCS1_OAEP.new(private_key)
+                    # print("Dec cipher rsa:", dec_cipher_rsa, '\n')
+                    self.dec_symm_key = dec_cipher_rsa.decrypt(message)
+                    # write to file so client can access it whenever they need
+                    f = open("symm_key.txt", "a")
+                    # print("Decrypted message:", dec_symm_key, '\n')
+                    # print(f"Decrypted symmetric key: {self.dec_symm_key}")
                     break
-        except Exception as e:
-            print(f"An error occurred while sending messages: {e}")
-            break
+            except Exception as e:
+                 print(f"An error occurred while receiving the symmetric key: {e}")
+                 break
+        # wait for messages/chat
+        while True:
+            try:
+                message = client_socket.recv(1024).decode()
+                
+                if message:
+                    print("\n" + message)  # Print the incoming message
+                    print("Secure-Chat> ", end='', flush=True)  # Reprint the prompt
+                else:
+                    break
+            except Exception as e:
+                print(f"An error occurred while receiving messages: {e}")
+                break
 
-    client_socket.close()
+    def send_messages(self, client_socket):
+        while True:
+            try:
+                user_input = input("Secure-Chat> ")
+                if user_input:
+                    client_socket.send(user_input.encode())
+                    if user_input == "3":  # Disconnect command
+                        break
+            except Exception as e:
+                print(f"An error occurred while sending messages: {e}")
+                break
 
-def start_client(server_name, server_port):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((server_name, server_port))
+        client_socket.close()
 
-    # private_key = client_socket.recv(2048)
+    def start_client(self,server_name, server_port):
+        keys = RSA.generate(2048)
+        pub_key = keys.publickey()
+        pem_pub_key = pub_key.export_key()
+        private_key = keys.export_key()
+        
+        #generate unique number for file name
+        unique_number = random.randint(1000, 9999)
+        while True:
+            with open("pubkey_file_number.txt", "r") as f:
+                for line in f:
+                    if unique_number == line:
+                        unique_number = random.randint(1000, 9999)
+                        break
+                break
+        str_unique_number = str(unique_number)
+        
+        #write the file number to file
+        with open("pubkey_file_number.txt", "a") as f:
+            f.write(f"{str_unique_number} ")
+        
+        #create file name for client pub key and write to it
+        file_name = str_unique_number + "public_key.pem"
+        with open(file_name, "wb") as file:
+            file.write(pem_pub_key)
+        
+        #create file name for client priv key and write to it
+        priv_file_name = str(unique_number) + "private_key.pem"
+        with open(priv_file_name, "wb") as file:
+            file.write(private_key)
 
-    keys = RSA.generate(2048)
-    pub_key = keys.publickey()
-    priv_key = keys.export_key()
-    with open("rsa_public_keys.txt", "a") as f:
-        f.write(f"{client_socket}  {pub_key}\n")
-    # Start a thread to listen for messages from the server
-    receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
-    receive_thread.start()
-  
-    # Handle sending messages in the main thread
-    send_messages(client_socket)
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((server_name, server_port))
+
+        # Start a thread to listen for messages from the server
+        receive_thread = threading.Thread(target=self.receive_messages, args=(client_socket,priv_file_name,))#str_unique_number))
+        receive_thread.start()
+    
+        # Handle sending messages in the main thread
+        self.send_messages(client_socket)
 
 if __name__ == "__main__":
+    client = Client()
     server_name = "127.0.0.1"
     server_port = 12345
-    start_client(server_name, server_port)
+    client.start_client(server_name, server_port)
